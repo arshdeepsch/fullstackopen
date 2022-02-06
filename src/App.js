4fbe124a2f3a3c7930986a84a253afb63010f6c2
Arshdeep Chudda <arshdeepsch@gmail.com>
2022-02-06 17:44:53 -0500

    Completed 2.11

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './Components/Filter'
 import PersonForm from './Components/PersonForm'
 import Persons from './Components/Persons'
+import axios from 'axios'
 
 
 const App = (props) => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
-  const [filtered, setFiltered] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [filtered, setFiltered] = useState([])
+
+  useEffect(() => {
+    console.log('in effect')
+    axios.get('http://localhost:3001/persons').then((resp) => { setPersons(resp.data); setFiltered(resp.data) })
+  }, [])
 
   return (
     <div>
