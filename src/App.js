93d2de1dc00e76d1fc6419c89459606aa1debb3c
Arshdeep Chudda <arshdeepsch@gmail.com>
2022-02-06 20:44:43 -0500

    Finished up 2.13

--- src/App.js
+++ src/App.js
@@ -1,36 +1,91 @@
 import React, { useState, useEffect } from 'react'
-import Filter from './Components/Filter'
-import PersonForm from './Components/PersonForm'
-import Persons from './Components/Persons'
 import axios from 'axios'
 
+const Languages = ({ country }) => {
+  const langKeys = Object.keys(country.languages)
+  return (
+    <div>
+      <h3>languages</h3>
+      <ul>
+        {langKeys.map((key) => <li key={key}> {country.languages[key]} </li>)}
+      </ul>
+    </div>
+  )
+}
+
+const ShowButton = ({ country, filtData, setFiltData }) => {
+  const handleShowClick = (event) => {
+    const spec = filtData.filter((country) => (country.name.common == event.target.name))
+    setFiltData(spec)
+  }
+
+  return (
+    <button name={country.name.common} onClick={handleShowClick}>show</button>
+  )
+}
+
+const Flag = ({ country }) => {
+  return (
+    <div>
+      <img src={country.flags.png}></img>
+    </div>
+  )
+}
+
+const Filter = ({ data, filtData, setFiltData }) => {
+  if (data.length > 10) {
+    return (
+      <div>
+        Too many matches, specify more
+      </div>
+    )
+  }
+  else if (data.length === 1) {
+    return (
+      <div>
+        <h1>{data[0].name.common}</h1>
+        <div>capital:    {data[0].capital} </div>
+        <div>population: {data[0].population}</div>
+        <Languages country={data[0]} />
+        <Flag country={data[0]} />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        {/* {data.map((country) => <div key={country.cca2} >{country.name.common}</div>)} */}
+        {data.map((country) => <div key={country.cca2}>{country.name.common}  <ShowButton country={country} setFiltData={setFiltData} filtData={filtData} /> </div>)}
+      </div>
+    )
+  }
+}
 
 const App = (props) => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNum, setNewNum] = useState('')
-  const [filtered, setFiltered] = useState([])
+  const [data, setData] = useState([])
+  const [filtData, setFiltData] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
-    console.log('in effect')
-    axios.get('http://localhost:3001/persons').then((resp) => { setPersons(resp.data); setFiltered(resp.data) })
+    axios.get('https://restcountries.com/v3.1/all').then((resp) => {
+      setData(resp.data);
+      setFiltData(resp.data)
+    })
   }, [])
+
+  const handleChange = (event) => {
+    setSearch(event.target.value)
+    const filtered = data.filter(
+      (country) => {
+        return country.name.common.toLowerCase().trim().includes(search.toLowerCase().trim())
+      }
+    )
+    setFiltData(filtered)
+  }
 
   return (
     <div>
-      <h1>Phonebook</h1>
-      <Filter persons={persons} setFiltered={setFiltered} />
-      <PersonForm
-        persons={persons}
-        setPersons={setPersons}
-        setNewName={setNewName}
-        setNewNum={setNewNum}
-        setFiltered={setFiltered}
-        newName={newName}
-        newNum={newNum}
-      />
-      <h2>Numbers</h2>
-      <Persons filtered={filtered} />
+      find countries <input onChange={handleChange}></input>
+      <Filter data={filtData} setFiltData={setFiltData} filtData={filtData} />
     </div>
   )
 }
