685770b804a156004c4464858a3fa156e5357acb
Arshdeep Chudda <arshdeepsch@gmail.com>
2022-02-11 13:57:46 -0500

    set up and finished up to 3.3

---
+++ part3/index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+
+const persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const dateObj = new Date()
+    res.send(`<div>Phonebook has info for ${persons.length} people</div> 
+        <div>${dateObj}</div>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const person = persons.filter(person => person.id === Number(req.params.id))
+    if (person.length > 0) {
+        res.json(person)
+    }
+    else {
+        res.status(404).end()
+    }
+})
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
